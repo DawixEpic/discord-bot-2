429a3a12b6398ac2268c3086f64a56833672def6
DawixEpic <dawidplaczek11@gmail.com>
2025-05-31 14:32:14 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+import os
 
 intents = discord.Intents.default()
+intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
@@ -18,7 +20,13 @@ async def on_ready():
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message("Pong!")
 
-import os
-TOKEN = os.getenv("DISCORD_TOKEN")
+@bot.tree.error
+async def on_app_command_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
+    await interaction.response.send_message(f"Wystąpił błąd: {error}", ephemeral=True)
 
-bot.run(TOKEN)
+TOKEN = os.getenv("DISCORD_TOKEN")
+if TOKEN is None:
+    raise ValueError("Token bota nie został ustawiony. Ustaw zmienną środowiskową DISCORD_TOKEN.")
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
